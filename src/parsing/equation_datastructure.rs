09f7b5443e983dc94b9091042508aec19ee24d80
Stefan <svill33@gmail.com>
2024-09-23 15:58:30 +0200

    feat: added datastructure for parsing opb files

---
+++ src/parsing/equation_datastructure.rs
@@ -0,0 +1,37 @@
+use bimap::{BiHashMap, BiMap};
+
+pub struct OPBFile {
+    pub name_map: BiMap<String, u32>,
+    pub equations: Vec<Equation>,
+    pub max_name_index: u32
+}
+
+impl OPBFile {
+    pub fn new() -> OPBFile {
+        OPBFile {
+            name_map: BiHashMap::new(),
+            equations: Vec::new(),
+            max_name_index: 0
+        }
+    }
+}
+pub struct Equation {
+    pub lhs: Vec<Summand>,
+    pub rhs: i32,
+    pub kind: EquationKind
+}
+
+pub enum EquationKind {
+    Eq,
+    Ge,
+    Le
+}
+
+pub struct Summand {
+    pub variable_index: u32,
+    pub factor: i32,
+    pub positive: bool
+}
+
+
+
