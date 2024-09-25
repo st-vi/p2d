cfe41ceb0a065d025690f14b58cd6ac929d26b96
Stefan <svill33@gmail.com>
2024-09-25 17:30:39 +0200

    feat: non recursive algorithm

--- src/solving/pseudo_boolean_datastructure.rs
+++ src/solving/pseudo_boolean_datastructure.rs
@@ -1,22 +1,26 @@
 use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{HashSet};
+use std::hash::{DefaultHasher, Hash, Hasher};
 use crate::parsing::equation_datastructure::{Equation, EquationKind, OPBFile, Summand};
 use crate::parsing::equation_datastructure::EquationKind::{Eq, Le};
-use crate::solving::pseudo_boolean_datastructure::PropagationResult::{ImpliedLiteral, LiteralNotInConstraint, Satisfied, Unsatisfied};
-use crate::solving::solver::Solver;
+use crate::solving::pseudo_boolean_datastructure::PropagationResult::{AlreadySatisfied, ImpliedLiteral, NothingToPropagated, Satisfied, Unsatisfied};
 
+#[derive(Clone)]
 pub struct PseudoBooleanFormula {
     pub constraints: Vec<Constraint>,
     pub number_variables: u32,
-    pub constraints_by_variable: Vec<Vec<usize>>
+    pub constraints_by_variable: Vec<HashSet<usize>>
 }
-
+#[derive(Clone)]
 pub struct Constraint {
     literals: Vec<Option<Literal>>,
     unassigned_literals: HashSet<Literal>,
     degree: i32,
     sum_true: u32,
     sum_unassigned: u32,
+    assignments: HashSet<(u32, bool)>,
+    factor_sum: u32,
+    //TODO cashe sum of max literal and update when necessary instead of calculating every time
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
@@ -30,7 +34,8 @@ pub enum PropagationResult {
     Satisfied,
     Unsatisfied,
     ImpliedLiteral(Literal),
-    LiteralNotInConstraint
+    NothingToPropagated,
+    AlreadySatisfied
 }
 
 impl PartialOrd for Literal {
@@ -60,23 +65,26 @@ impl PseudoBooleanFormula {
         let mut pseudo_boolean_formula = PseudoBooleanFormula{
             constraints: Vec::new(),
             number_variables: opb_file.max_name_index,
-            constraints_by_variable: Vec::with_capacity(opb_file.max_name_index as usize)
+            constraints_by_variable: Vec::with_capacity((opb_file.max_name_index - 1)as usize)
         };
 
-        for i in 0..opb_file.max_name_index{
-            pseudo_boolean_formula.constraints_by_variable.push(Vec::new());
+        for _ in 0..opb_file.max_name_index{
+            pseudo_boolean_formula.constraints_by_variable.push(HashSet::new());
         }
 
         let mut constraint_counter = 0;
         for equation in equation_list {
+            //TODO set rhs to 0 if it is negative
             let mut constraint = Constraint {
                 degree: equation.rhs,
                 sum_true: 0,
                 sum_unassigned: equation.lhs.iter().map(|s| s.factor).sum::<i32>() as u32,
                 literals: Vec::with_capacity((opb_file.max_name_index - 1) as usize),
                 unassigned_literals: HashSet::new(),
+                assignments: HashSet::new(),
+                factor_sum: equation.lhs.iter().map(|s| s.factor).sum::<i32>() as u32
             };
-            for i in 0..opb_file.max_name_index{
+            for _ in 0..opb_file.max_name_index{
                 constraint.literals.push(None);
             }
             for summand in equation.lhs {
@@ -88,7 +96,7 @@ impl PseudoBooleanFormula {
                     index: summand.variable_index,
                     factor: summand.factor as u32,
                     positive: summand.positive});
-                pseudo_boolean_formula.constraints_by_variable.get_mut(summand.variable_index as usize).unwrap().push(constraint_counter);
+                pseudo_boolean_formula.constraints_by_variable.get_mut(summand.variable_index as usize).unwrap().insert(constraint_counter);
             }
             pseudo_boolean_formula.constraints.push(constraint);
             constraint_counter += 1;
@@ -99,6 +107,12 @@ impl PseudoBooleanFormula {
 
 impl Constraint {
     pub fn propagate(&mut self, literal: Literal) -> PropagationResult {
+        if self.assignments.contains(&(literal.index, !literal.positive)) {
+            return Unsatisfied
+        }else if self.assignments.contains(&(literal.index, literal.positive)){
+            return NothingToPropagated
+        }
+        let already_satisfied =  self.sum_true >= self.degree as u32;
         let literal_in_constraint = self.literals.get(literal.index as usize).unwrap();
         match literal_in_constraint {
             None => {
@@ -109,26 +123,88 @@ impl Constraint {
                     //literal factor is taken
                     self.sum_true += literal_in_constraint.factor;
                     self.sum_unassigned -= literal_in_constraint.factor;
-                    if self.degree <= 0 {
-                        // fulfilled
-                        return Satisfied
-                    }else if self.sum_true + self.sum_unassigned < self.degree as u32 {
-                        // violated
-                        return Unsatisfied
-                    }else if self.sum_true + self.sum_unassigned < (self.degree as u32) + self.unassigned_literals.iter().max().unwrap().factor {
-                        //max literal implied
-                        let max_literal = self.unassigned_literals.iter().max().unwrap();
-                        return ImpliedLiteral(max_literal.clone())
-                    }
-                    unreachable!()
+                    self.unassigned_literals.retain(|x| x.index != literal.index);
+                    self.assignments.insert((literal.index, literal.positive));
                 }else{
                     //literal factor is not taken
                     self.sum_unassigned -= literal_in_constraint.factor;
                     self.unassigned_literals.retain(|x| x.index != literal.index);
-                    return LiteralNotInConstraint
+                    self.assignments.insert((literal.index, literal.positive));
                 }
+                if self.sum_true >= self.degree as u32 {
+                    // fulfilled
+                    return if already_satisfied {
+                        AlreadySatisfied
+                    } else {
+                        Satisfied
+                    }
+
+                }else if self.sum_true + self.sum_unassigned < self.degree as u32 {
+                    // violated
+                    self.unassigned_literals.retain(|x| x.index != literal.index);
+                    self.assignments.insert((literal.index, literal.positive));
+                    return Unsatisfied
+                }else if self.sum_true + self.sum_unassigned < (self.degree as u32) + self.unassigned_literals.iter().max().unwrap().factor {
+                    //max literal implied
+                    let max_literal = self.unassigned_literals.iter().max().unwrap().clone();
+                    let return_value = ImpliedLiteral(max_literal.clone());
+                    return return_value;
+                }
+                NothingToPropagated
+
             }
         }
+    }
+
+    pub fn undo(&mut self, variable_index: u32, variable_sign: bool) -> bool {
+        if let Some(literal) = self.literals.get(variable_index as usize).unwrap() {
+            let satisfied_before_undo = self.sum_true >= self.degree as u32;
+            self.unassigned_literals.insert(literal.clone());
+            self.assignments.remove(&(variable_index, variable_sign));
+            self.sum_unassigned += literal.factor;
+            if literal.positive == variable_sign {
+                self.sum_true -= literal.factor;
+            }
+            let satisfied_after_undo = self.sum_true >= self.degree as u32;
+            if satisfied_before_undo && !satisfied_after_undo {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn simplify(&mut self) -> PropagationResult {
+        if self.sum_true >= self.degree as u32 {
+            // fulfilled
+                return Satisfied
+        }else if self.sum_true + self.sum_unassigned < self.degree as u32 {
+            // violated
+            return Unsatisfied
+        }else if self.sum_true + self.sum_unassigned < (self.degree as u32) + self.unassigned_literals.iter().max().unwrap().factor {
+            //max literal implied
+            let max_literal = self.unassigned_literals.iter().max().unwrap().clone();
+            let return_value = ImpliedLiteral(max_literal.clone());
+            self.unassigned_literals.remove(&max_literal);
+            return return_value;
+        }
+        NothingToPropagated
+    }
+
+    pub fn is_unsatisfied(&self) -> bool {
+        self.sum_true < self.degree as u32
+    }
+
+    pub fn hash(&self, x: &mut DefaultHasher) -> u64 {
+        let mut s = DefaultHasher::new();
+        self.literals.hash(&mut s);
+        for item in &self.assignments {
+            item.hash(&mut s);
+        }
+        for item in &self.unassigned_literals {
+            item.hash(&mut s);
+        }
+        self.degree.hash(&mut s);
+        s.finish()
     }
 }
 
--- src/solving/solver.rs
+++ src/solving/solver.rs
@@ -1,60 +1,265 @@
-use std::collections::VecDeque;
+use std::cmp::PartialEq;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::{DefaultHasher, Hash, Hasher};
 use crate::solving::pseudo_boolean_datastructure::{Constraint, Literal, PseudoBooleanFormula};
 use crate::solving::pseudo_boolean_datastructure::PropagationResult::*;
+use crate::solving::solver::AssignmentKind::{FirstDecision, Propagated, SecondDecision};
 
 pub struct Solver {
     pseudo_boolean_formula: PseudoBooleanFormula,
-    assignment_stack: Vec<u32>,
+    assignment_stack: Vec<Assignment>,
     decision_level: u32,
     learned_clauses: Vec<Constraint>,
-    pub propagation_queue: VecDeque<Literal>,
     result_stack: Vec<u128>,
+    number_unsat_constraints: usize,
+    unassigned_variables: HashSet<u32>,
+    model_counter: u128,
+    cache: HashMap<u64,(u128,PseudoBooleanFormula)>,
 }
 
 impl Solver {
     pub fn new(pseudo_boolean_formula: PseudoBooleanFormula) -> Solver {
+        let number_unsat_constraints = pseudo_boolean_formula.constraints.len();
+        let mut unassigned_variables = HashSet::new();
+        for i in 0..pseudo_boolean_formula.number_variables{
+            unassigned_variables.insert(i);
+        }
         Solver {
             pseudo_boolean_formula,
             assignment_stack: Vec::new(),
             decision_level: 0,
             learned_clauses: Vec::new(),
-            propagation_queue: VecDeque::new(),
             result_stack: Vec::new(),
+            number_unsat_constraints,
+            unassigned_variables,
+            model_counter: 0,
+            cache: HashMap::new()
         }
     }
 
     pub fn solve(&mut self) -> u128 {
+        if !self.simplify(){
+            return 0;
+        }
         loop {
-            self.propagate();
+            if self.number_unsat_constraints <= 0 {
+                self.result_stack.push(2_u128.pow(self.unassigned_variables.len() as u32));
+                self.model_counter += 2_u128.pow(self.unassigned_variables.len() as u32);
+                if !self.backtrack(){
+                    //nothing to backtrack to, we searched the whole space
+                    return self.result_stack.pop().unwrap();
+                }
+                continue
+            }
+            let cached_result = self.get_cached_result();
+            match cached_result {
+                None => {
+                    self.decide();
+                    let last_assignment = self.assignment_stack.last().unwrap();
+                    if !self.propagate(last_assignment.variable_index, last_assignment.variable_sign){
+                        //at least one constraint violated
+                        self.result_stack.push(0);
+                        if !self.backtrack(){
+                            //nothing to backtrack to, we searched the whole space
+                            return self.result_stack.pop().unwrap();
+                        }
+                    }
+                },
+                Some(c) => {
+                    self.result_stack.push(c);
+                    self.model_counter += c;
+                    if !self.backtrack(){
+                        //nothing to backtrack to, we searched the whole space
+                        return self.model_counter;
+                    }
+                    continue
+                }
+            }
 
         }
-        0
     }
 
-    fn propagate(&mut self) -> bool {
-        while !self.propagation_queue.is_empty() {
-            let literal = self.propagation_queue.pop_front().unwrap();
-            for constraint_index in self.pseudo_boolean_formula.constraints_by_variable.get(literal.index as usize).unwrap() {
-                let result = self.pseudo_boolean_formula.constraints.get_mut(*constraint_index).unwrap().propagate(literal.clone());
+    fn propagate(&mut self, variable_index: u32, variable_sign: bool) -> bool {
+        let mut propagation_queue:VecDeque<(u32, bool)> = VecDeque::new();
+        propagation_queue.push_back((variable_index, variable_sign));
+        while !propagation_queue.is_empty() {
+            let (index, sign) = propagation_queue.pop_front().unwrap();
+            self.unassigned_variables.remove(&index);
+            for constraint_index in self.pseudo_boolean_formula.constraints_by_variable.get(index as usize).unwrap() {
+                let result = self.pseudo_boolean_formula.constraints.get_mut(*constraint_index).unwrap().propagate(Literal{index, positive: sign, factor: 0});
                 match result {
                     Satisfied => {
-
+                        self.number_unsat_constraints -= 1;
                     },
                     Unsatisfied => {
-                        self.propagation_queue.clear();
+                        propagation_queue.clear();
                         return false;
                     },
                     ImpliedLiteral(l) => {
-                        self.propagation_queue.push_back(l);
+                        let assignment = Assignment{
+                            variable_sign: l.positive,
+                            variable_index: l.index,
+                            decision_level: self.decision_level,
+                            assignment_kind: Propagated
+                        };
+                        propagation_queue.push_back((l.index, l.positive));
+                        self.assignment_stack.push(assignment);
                     },
-                    LiteralNotInConstraint => {}
+                    NothingToPropagated => {},
+                    AlreadySatisfied => {}
                 }
             }
         }
         true
     }
 
-    fn decide(&self){
+    fn decide(&mut self){
+        self.decision_level += 1;
+        //TODO better heuristic than smallest index?
+        let variable_index = *self.unassigned_variables.iter().min().unwrap();
+        self.unassigned_variables.remove(&variable_index);
+        self.assignment_stack.push(Assignment{
+            assignment_kind: AssignmentKind::FirstDecision,
+            variable_sign: true,
+            decision_level: self.decision_level,
+            variable_index
+        });
+    }
 
+    fn backtrack(&mut self) -> bool {
+        loop {
+            if let Some(top_element) = self.assignment_stack.last(){
+                if top_element.decision_level == 0{
+                    return false;
+                }else if top_element.assignment_kind == Propagated {
+                    self.undo_assignment(top_element.variable_index, top_element.variable_sign);
+                    self.assignment_stack.pop();
+                }else if top_element.assignment_kind == FirstDecision {
+                    self.undo_assignment(top_element.variable_index, top_element.variable_sign);
+                    let new_sign = !self.assignment_stack.last().unwrap().variable_sign;
+                    self.assignment_stack.last_mut().unwrap().variable_sign = new_sign;
+                    self.assignment_stack.last_mut().unwrap().assignment_kind = SecondDecision;
+                    self.decision_level = self.assignment_stack.last_mut().unwrap().decision_level;
+                    let last_assignment = self.assignment_stack.last().unwrap();
+                    self.propagate(last_assignment.variable_index, last_assignment.variable_sign);
+                    return true;
+                }else if top_element.assignment_kind == SecondDecision {
+                    let r1 = self.result_stack.pop().unwrap();
+                    let r2 = self.result_stack.pop().unwrap();
+                    self.result_stack.push(r1+r2);
+                    let top_index = top_element.variable_index;
+                    let top_sign = top_element.variable_sign;
+                    self.undo_assignment(top_index, top_sign);
+                    self.assignment_stack.pop();
+                    self.cache();
+                }
+            }else {
+                return false;
+            }
+
+        }
+    }
+
+    fn undo_assignment(&mut self, variable_index: u32, variable_sign: bool) {
+        self.unassigned_variables.insert(variable_index);
+        for constraint_index in self.pseudo_boolean_formula.constraints_by_variable.get(variable_index as usize).unwrap() {
+            let constraint = self.pseudo_boolean_formula.constraints.get_mut(*constraint_index).unwrap();
+            if constraint.undo(variable_index, variable_sign) {
+                self.number_unsat_constraints += 1;
+            }
+        }
+    }
+
+    fn simplify(&mut self) -> bool {
+        let mut propagation_set = HashSet::new();
+        for constraint in &mut self.pseudo_boolean_formula.constraints {
+            match constraint.simplify(){
+                Satisfied => {
+                    self.number_unsat_constraints -= 1;
+                },
+                Unsatisfied => {
+                    return false;
+                },
+                ImpliedLiteral(l) => {
+                    self.assignment_stack.push(Assignment{
+                        variable_index: l.index,
+                        assignment_kind: Propagated,
+                        decision_level: 0,
+                        variable_sign: l.positive,
+                    });
+                    propagation_set.insert((l.index, l.positive));
+                }
+                _ => {}
+            }
+        }
+        for (index, sign) in propagation_set {
+            if !self.propagate(index, sign){
+                return false;
+            }
+        }
+        true
+    }
+
+    fn hash_state(&self) -> u64 {
+        let mut s = DefaultHasher::new();
+
+        for c in &self.pseudo_boolean_formula.constraints {
+            if c.is_unsatisfied() {
+                c.hash(&mut s);
+            }
+        }
+
+        s.finish()
+    }
+
+    fn cache(&mut self) {
+        if self.number_unsat_constraints > 0 {
+            self.cache.insert(self.hash_state(), (*self.result_stack.last().unwrap(), self.pseudo_boolean_formula.clone()));
+        }
+    }
+
+    fn get_cached_result(&self) -> Option<u128> {
+        match self.cache.get(&self.hash_state()) {
+            None => None,
+            Some((c,_)) => Some(*c)
+        }
+    }
+}
+
+struct Assignment {
+    decision_level: u32,
+    variable_index: u32,
+    variable_sign: bool,
+    assignment_kind: AssignmentKind
+}
+
+#[derive(PartialEq)]
+enum AssignmentKind {
+    Propagated,
+    FirstDecision,
+    SecondDecision
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parsing;
+    use super::*;
+
+    #[test]
+    fn test_ex_1() {
+        let opb_file = parsing::parser::parse("x1 + x2 >= 0;\n3 x2 + x3 + x4 + x5 >= 3;").expect("error while parsing");
+        let formula = PseudoBooleanFormula::new(&opb_file);
+        let mut solver = Solver::new(formula);
+        let model_count = solver.solve();
+        assert_eq!(model_count, 18);
+    }
+
+    #[test]
+    fn test_ex_2() {
+        let opb_file = parsing::parser::parse("x1 + x2 >= 1;\n3 x2 + x3 + x4 + x5 >= 3;").expect("error while parsing");
+        let formula = PseudoBooleanFormula::new(&opb_file);
+        let mut solver = Solver::new(formula);
+        let model_count = solver.solve();
+        assert_eq!(model_count, 17);
     }
 }
